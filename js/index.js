7645a0320253742a4a1fa42adc7e9f872e8f5403
Alex <secretarius@ukr.net>
2019-02-10 23:18:12 +0000

    cr some json

--- js/index.js
+++ js/index.js
@@ -32,3 +32,19 @@ function ajaxGetJson(){
     xhr.send();
 }
 
+window.addEventListener('load', getCurrency)
+document.querySelector('.update-currency')
+    .addEventListener('click', getCurency)
+
+function getCurrency(){
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState ===4 && xhr.status===200){
+            const currency = JSON.parse(xhr.responseText);
+            document.querySelector('.currency-continer')
+                .innerText = currency.USD_UAH.val;
+        }
+    }
+    xhr.open('GET', 'https://free.currencyconverterapi.com/api/v6/convert?q=USD_UAH&compact=y', true)
+    xhr.send();
+}
