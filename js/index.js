32dfdb186c7130d4cf9b1e959a7cd919bc0e8d96
Alex <secretarius@ukr.net>
2019-02-10 09:07:04 +0000

    cr ajax with json

--- js/index.js
+++ js/index.js
@@ -15,3 +15,20 @@ function ajaxGetHtml(){
     xhr.open('GET', 'client-data.html', true);
     xhr.send();
 }
+
+document.querySelector('.ajax-get-json')
+    .addEventListener('click', ajaxGetJson)
+
+function ajaxGetJson(){
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState ===4 && xhr.status===200){
+            const clientData = JSON.parse(xhr.responseText);
+            document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.client-account').innerText = clientData.account;
+        }
+    }
+    xhr.open('GET', 'client-data.json', true)
+    xhr.send();
+}
+
